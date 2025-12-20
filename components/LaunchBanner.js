eb08d9d9ed70b070c7910e86fccbfd9882374cdf
Obinna Okere <okereobinna10@gmail.com>
2025-12-20 23:41:39 +0100

    Start Here page added and rotation animation effect added to Launch Banner

--- components/LaunchBanner.js
+++ components/LaunchBanner.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import style from '../styles/launchbanner.module.css'
 
 function LaunchBanner() {
   return (
@@ -10,11 +12,11 @@ function LaunchBanner() {
             Ready To Launch Something <span className='font-bold'>Extraordinary?</span>
           </div>
           <div className='text-[#FFFFFF] flex gap-2 items-center justify-center cursor-pointer border-2 border-[#B11A3D] px-[40px] py-[10px] rounded-[12px] mt-[10px] w-[100%] '>
-              <div>Start Here</div> 
+              <Link href={'/start-here'} className='cursor-pointer'>Start Here</Link>
           </div>
         </div>
         <div className='flex flex-col  items-center justify-center text-white w-[60%] h-full  py-[40px] px-[50px]'>
-          <Image src={'/spiral.png'} width={900} height={1000} alt='Launch Banner Image' className='absolute object-cover mr-[-270px]' />
+          <Image src={'/spiral.png'} width={900} height={1000} alt='Launch Banner Image' className={`${style.spiral} absolute object-cover mr-[-270px]`} />
         </div>
       </div>
     </div>
