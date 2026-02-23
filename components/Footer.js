cb43c6f22b44c3e612f864bd413ecffbb31faca3
Obinna Okere <okereobinna10@gmail.com>
2026-02-23 22:59:24 +0100

    Footer Section now mobile responsive

--- components/Footer.js
+++ components/Footer.js
@@ -7,13 +7,23 @@ function Footer() {
     const currentYear = new Date().getFullYear();
 
   return (
-    <div className='flex flex-row gap-3 items-center justify-between w-full min-h-[100px] border-2 border-[#4b4b4b]'>
+    <div className='flex flex-col xl:flex-row lg:flex-row
+    gap-[40px] xl:gap-3 lg:gap-3 items-center justify-between 
+    w-full 
+    h-full lg:min-h-[100px] xl:min-h-[100px] 
+    py-[30px]
+    px-[15px] lg:px-[50px] xl:px-[50px]
+    pb-[100px]
+    border-2 border-[#4b4b4b]'>
         
         {/* Logo Here */}
-        <div className='text-white text-[14px] pt-5 flex flex-col pl-[50px] gap-3'>
+        <div className='text-white text-[14px] pt-5 flex flex-col  gap-3 w-full lg:w-[33%] xl:w-[33%]'>
             <Link href={'/'}>
-                <Image className='cursor-pointer' src="/logofooter.png" alt="social media links" width={150} height={30} />
+                <Image className='cursor-pointer' src="/logofooter.png" alt="social media links" width={170} height={30} />
             </Link>
+            <section>
+                Digital agency specializing in branding, digital marketing, web & app development, and growth-driven creative solutions.
+            </section>
             <div>
                  © {currentYear} All rights reserved.
             </div>
@@ -21,14 +31,14 @@ function Footer() {
 
         {/* Privacy Policy . Terms & Conditions . Refund Policy */}
 
-        <div className='flex flex-row items-center justify-center gap-5 text-[#FFFFFF] text-[14px]'>
+        <div className='flex flex-col xl:flex-row lg:flex-row items-left xl:items-center lg:items-center justify-center gap-5 text-[#FFFFFF] text-[14px] w-full lg:w-[33%] xl:w-[33%]'>
             <Link href={'/privacy'} className='cursor-pointer hover:underline hover:text-[#B11A3D]'>Privacy Policy</Link>
             <Link href={'/termsandconditions'} className='cursor-pointer hover:underline hover:text-[#B11A3D]'>Terms & Conditions</Link>
             <Link href={'/refund'} className='cursor-pointer hover:underline hover:text-[#B11A3D]'>Refund Policy</Link>
         </div>
 
         {/* Social Media Links Here */}
-        <div className='flex flex-row gap-[20px] items-center pr-[50px]'>
+        <div className='flex flex-row gap-[20px] items-center justify-center  w-full lg:w-[33%] xl:w-[33%]'>
             <Image className='cursor-pointer' src="/instagram.png" alt="social media links" width={20} height={30} />
             <Image className='cursor-pointer' src="/tiktok.png" alt="social media links" width={20} height={30} />
             <Image className='cursor-pointer' src="/linkedin.png" alt="social media links" width={20} height={30} />
