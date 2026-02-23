d220d3ef1e1763f10edbdae877b3aaa5df248c35
Obinna Okere <okereobinna10@gmail.com>
2026-02-23 14:10:07 +0100

    Launch Banner now mobile responsive

--- components/LaunchBanner.js
+++ components/LaunchBanner.js
@@ -5,9 +5,9 @@ import style from '../styles/launchbanner.module.css'
 
 function LaunchBanner() {
   return (
-    <div className='flex flex-col gap-3 items-center justify-center w-full xl:w-[80%] h-[400px]'>
-      <div className='flex flex-row gap-3 w-[80%] xl:w-[80%] bg-[#2D2D30] h-[400px] rounded-[2em]  overflow-hidden relative'>
-        <div className='flex flex-col  items-center justify-center text-white w-[40%] h-full py-[40px] px-[50px]'>
+    <div className='flex flex-col gap-3 items-center justify-center w-full xl:w-[80%] h-full xl:h-[400px] lg:h-[400px] px-[15px] xl:px-[0px] lg:px-[0px]'>
+      <div className='flex flex-col-reverse xl:flex-row lg:flex-row gap-3 w-full xl:w-[80%] lg:w-[80%] bg-[#2D2D30] h-full xl:h-[400px] lg:h-[400px] rounded-[2em] overflow-hidden relative'>
+        <div className='flex flex-col items-center justify-center text-white w-full xl:w-[40%] lg:w-[40%] h-full py-[40px] px-[50px]'>
           <div className='text-[30px]'>
             Ready To Launch Something <span className='font-bold'>Extraordinary?</span>
           </div>
@@ -15,8 +15,8 @@ function LaunchBanner() {
               <Link href={'/start-here'} className='cursor-pointer'>Start Here</Link>
           </div>
         </div>
-        <div className='flex flex-col  items-center justify-center text-white w-[60%] h-full  py-[40px] px-[50px]'>
-          <Image src={'/spiral.png'} width={900} height={1000} alt='Launch Banner Image' className={`${style.spiral} absolute object-cover mr-[-270px]`} />
+        <div className='flex flex-col  items-center justify-center text-white w-full xl:w-[60%] lg:w-[60%] h-full  py-[40px] px-[50px]'>
+          <Image src={'/spiral.png'} width={900} height={1000} alt='Launch Banner Image' className={`${style.spiral} absolute object-cover mr-[-200px] xl:mr-[-270px] lg:mr-[-270px]`} />
         </div>
       </div>
     </div>
